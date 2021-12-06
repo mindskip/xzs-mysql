5e14a641ac537b005b9bea33b4120c58a0e6c1c0
alvis <alvis_u@qq.com>
2021-12-06 09:49:10 +0800

    修复部分机器接口不兼容问题

--- source/xzs/src/main/java/com/mindskip/xzs/repository/UserMapper.java
+++ source/xzs/src/main/java/com/mindskip/xzs/repository/UserMapper.java
@@ -15,21 +15,6 @@ import java.util.Map;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
-
-
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(User record);
-
-    int insertSelective(User record);
-
-    User selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(User record);
-
-    int updateByPrimaryKey(User record);
-
-
     /**
      * getAllUser
      *
