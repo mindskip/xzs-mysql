58b66422422bc27226d9dc67b6a220d381384168
Leo <3509836544@qq.com>
2023-09-02 22:23:21 +0800

    修复了XSS注入的BUG，改进了特殊字符过滤方式

--- source/xzs/src/main/java/com/mindskip/xzs/utility/HtmlUtil.java
+++ source/xzs/src/main/java/com/mindskip/xzs/utility/HtmlUtil.java
@@ -1,5 +1,8 @@
 package com.mindskip.xzs.utility;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,6 +20,8 @@ public class HtmlUtil {
      * @param htmlStr the html str
      * @return the string
      */
+
+    @Deprecated
     public static String clear(String htmlStr) {
         String regEx_script = "<script[^>]*?>[\\s\\S]*?<\\/script>";
         String regEx_style = "<style[^>]*?>[\\s\\S]*?<\\/style>";
@@ -32,4 +37,62 @@ public class HtmlUtil {
         htmlStr = m_html.replaceAll("");
         return htmlStr.trim();
     }
+
+//    上面的代码存在严重的问题，因此需要改动
+//    存在XSS注入
+
+    public static interface EscapedString {
+        String getAlertText();
+
+        String getPattern();
+
+        String alert(String text);
+    }
+
+    private static class SimpleEscapedString implements EscapedString {
+        private final String pattern;// 匹配字符
+
+        private final Pattern p;// 一次编译，多次使用，可以减少编译的成本
+        private final String alertText;// 替换字符
+
+        public String getAlertText() {
+            return alertText;
+        }
+
+        public String getPattern() {
+            return pattern;
+        }
+
+        SimpleEscapedString(String pattern, String alertText) {
+            this.pattern = Objects.requireNonNull(pattern);
+            this.alertText = Objects.requireNonNull(alertText);
+            this.p = Pattern.compile(pattern);
+        }
+
+        public String alert(String text) {
+            if (Objects.requireNonNull(text).isEmpty()) {
+                return text;
+            }
+            return p.matcher(text).replaceAll(alertText);
+        }
+    }
+
+    public static final List<EscapedString> escape = new ArrayList<EscapedString>();
+
+    static {
+//        这里的$0时回溯引用，这里的处理就是对匹配到的字符加上前后缀
+//        测试用例：      <script>wuhu<script>
+//                      <alt><</alt>script<alt>></alt>wuhu<alt><</alt>script<alt>></alt>
+        escape.add(new SimpleEscapedString("<|>", "<alt>$0</alt>"));
+    }
+
+    public static String alert(String text) {
+        if (Objects.requireNonNull(text).isEmpty()) return text;
+        for (EscapedString escapedString : escape) {
+            text = escapedString.alert(text);
+        }
+        return text;
+    }
+
+
 }
