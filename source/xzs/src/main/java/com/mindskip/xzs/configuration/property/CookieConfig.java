5bc91ec826e814be1f63ad700add577d364ff472
alvis <alvis_u@qq.com>
2024-01-03 08:41:28 +0800

    2024

--- source/xzs/src/main/java/com/mindskip/xzs/configuration/property/CookieConfig.java
+++ source/xzs/src/main/java/com/mindskip/xzs/configuration/property/CookieConfig.java
@@ -3,7 +3,7 @@ package com.mindskip.xzs.configuration.property;
 /**
  * @version 3.5.0
  * @description: The type Cookie config.
- * Copyright (C), 2020-2021, 武汉思维跳跃科技有限公司
+ * Copyright (C), 2020-2024, 武汉思维跳跃科技有限公司
  * @date 2021/12/25 9:45
  */
 public class CookieConfig {
